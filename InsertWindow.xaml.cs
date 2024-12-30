f066455784268098eb0e12545fa2e683a19f524a
mha66 <mharegaily@gmail.com>
2024-12-30 14:23:39 +0200

    Add project files.

---
+++ InsertWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BankDatabaseGUI
+{
+    /// <summary>
+    /// Interaction logic for InsertWindow.xaml
+    /// </summary>
+    public partial class InsertWindow : Window
+    {
+        public InsertWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CreateAccBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CreateAccountWindow obj = new CreateAccountWindow();
+            obj.Show();
+            //this.Hide();
+        }
+
+        private void AddCardBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MakeTransactBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MakeTransferBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BankDatabaseGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void InsertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            InsertWindow obj = new InsertWindow();
+            obj.Show();
+            this.Hide();
+
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+
+        //Navigate to another form:
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    Form2 obj = new Form2();
+        //    obj.Show();
+        //    this.Hide();
+        //}
+    }
+}
